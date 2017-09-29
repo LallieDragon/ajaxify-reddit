239a07221936bc2c2d196f30a4cd7f7388db4d8e
Carrington Simecheck <c.simecheck89@yahoo.com>
2017-09-29 16:20:46 -0500

    work in progress, rewrite on everything

--- js/main.js
+++ js/main.js
@@ -1,17 +1,44 @@
 /* GLOBAL VARIABLES UP HERE */
 var frontPage = 'https://www.reddit.com/.json';
-
-
-
-
+var postArray = [];
 
 $(document).ready(function(){
-/* FUNCTION EXECUTION HERE */
-  console.log('Go forth and code!');
+  function postCall(){
+    $.ajax({
+      method: 'GET',
+      url: frontPage,
+      data: 'json',
+      success: onSuccess/*,
+      error: onError*/
+    });
+  }
 
-});
+  function onSuccess(resource){
+    console.log("fuck");
+    resource.data.children.forEach (function (postData) {
+    postArray.push(new Post({
+      image: postData.data.thumbnail,
+      title: postData.data.title,
+      user: postData.data.author,
+      date: postData.data.created
+      }));
+    });
+  }
+
+  function Post(options = {}) {
+    this.image = options.image;
+    this.title = options.title;
+    this.user = options.user;
+    //make method for date shit
+    this.date = options.date;
+  }
+  postCall();
+  console.log(postArray)
+  Post.prototype.assignTitle = function () {
+    $('#image').append()
+
+  }
+})
 
 
 
-/* FUNCTION DEFINITION HERE */
-/* TIP: don't forget scope! */
